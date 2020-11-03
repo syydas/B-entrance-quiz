902da26e6a850399f2059045259f25949512cbb7
zhuyuqian <yuqian.zhu@thoughtworks.com>
2020-11-03 10:54:41 +0800

    [zhuyuqian]GTB comments

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -11,3 +11,9 @@ public class GtbEntranceQuizApplication {
 	}
 
 }
+
+// TODO GTB-完成度: * 只完成了查询学生列表的功能
+// TODO GTB-测试: * 没有包含任何测试
+// TODO GTB-知识点: * Spring IoC相关的注解使用合理
+// TODO GTB-知识点: * 完成度较低，无法判断知识点的掌握程度
+// TODO GTB-工程实践: * 项目分包合理，使用了三层架构
